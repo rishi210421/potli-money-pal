db1e9e7ed1a541b62309c24cfe2a2ac0a00108f7
rishi210421 <rishisenani009@gmail.com>
2026-06-18 00:45:02 +0530

    add potli logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,14 +1,7 @@
-import logoAsset from "@/assets/potli-logo.png.asset.json";
+import logo from "./potli-logo.png";
 
 export function Logo({ size = 96, withWordmark = false, className = "" }: { size?: number; withWordmark?: boolean; className?: string }) {
   return (
-    <img
-      src={logoAsset.url}
-      alt="Potli"
-      width={size}
-      height={withWordmark ? size : size}
-      style={{ width: size, height: "auto", objectFit: "contain" }}
-      className={className}
-    />
+<img src="/potli-logo.png" alt="Potli" />
   );
 }
